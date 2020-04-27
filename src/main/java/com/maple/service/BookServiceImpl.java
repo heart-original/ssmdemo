c9a8f3abd169fb6b38a4dcd3e5699110bcc32d1a
heart-original <1316305052@qq.com>
2020-04-27 19:32:00 +0800

    ssm  crud

---
+++ src/main/java/com/maple/service/BookServiceImpl.java
@@ -0,0 +1,36 @@
+package com.maple.service;
+
+import com.maple.dao.BookMapper;
+import com.maple.pojo.Books;
+
+import java.util.List;
+
+public class BookServiceImpl implements BookService {
+
+    //调用dao层的操作，设置一个set接口，方便Spring管理
+    private BookMapper bookMapper;
+
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
+    public int addBook(Books book) {
+        return bookMapper.addBook(book);
+    }
+
+    public int deleteBookById(int id) {
+        return bookMapper.deleteBookById(id);
+    }
+
+    public int updateBook(Books books) {
+        return bookMapper.updateBook(books);
+    }
+
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    public List<Books> queryAllBook() {
+        return bookMapper.queryAllBook();
+    }
+}
